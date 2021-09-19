5059afe7b7d7abef3d6c0036afe911e5074872b4
Mide141014 <91020636+Mide141014@users.noreply.github.com>
2021-09-19 16:18:25 +0100

    git clone https://github.com/Mide141014/TEST.git ls cd starryx ls python2 starryx.py
    My command for termux

---
+++ Mide.ex
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Mide141014
+- 👀 I’m interested in creating termux command
+- 🌱 I’m currently learning how to create termux command
+- 💞️ I’m looking to collaborate on command
+- 📫 How to reach me ...
+https://www.facebook.com/oshindele.mary.9216778
+<!---
+Mide141014/Mide141014 is a ✨ special ✨ repository because its `MIDE.ex` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
